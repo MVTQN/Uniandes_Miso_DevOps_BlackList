74b157ff9c8d1f2595f91e88aff07647ea699168
Afiffe Retamozo <afiffe.retamozo.a@gmail.com>
2023-04-15 11:38:24 -0500

    add new changes

--- app_blacklist/src/application.py
+++ app_blacklist/src/application.py
@@ -3,12 +3,13 @@ from .session import Session, engine
 from .models.model import Base
 from .blueprints.blacklist import blacklist_blueprint
 
-
-app = Flask(__name__)
-app.register_blueprint(blacklist_blueprint)
+application = Flask(__name__)
+application.register_blueprint(blacklist_blueprint)
 
 Base.metadata.create_all(engine)
 
+if __name__ == "__main__":
+    application.run(port = 5000, debug = True)
 #@app.errorhandler(ApiError)
 # def handle_exception(err):
 #     response = {
